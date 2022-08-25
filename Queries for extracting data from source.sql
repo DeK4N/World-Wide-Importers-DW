aa69fdb2052c5013cb3e6890ee88ca56341b7362
DeK4N <mzemla115@gmail.com>
2022-08-25 18:40:21 +0200

    Created querries for extracting data from source - Sales area

---
+++ Queries for extracting data from source.sql
@@ -0,0 +1,98 @@
+/*#####
+	Querry for extracting customers data -> dimCustomers
+####*/
+SELECT
+	MC.[CustomerID]
+	,MC.[CustomerName]
+	,PC.[CustomerName] [ParentCompany]
+	,CC.[CustomerCategoryName] [CustomerCategory]
+	,MC.[DeliveryCityID]
+	,MC.[PostalCityID]
+	,MC.[DeliveryAddressLine1]
+	,MC.[DeliveryAddressLine2]
+	,MC.[DeliveryPostalCode]
+	--,MC.[DeliveryLocation]
+	,MC.[PostalAddressLine1]
+	,MC.[PostalAddressLine2]
+	,MC.[PostalPostalCode]
+FROM Sales.Customers MC
+JOIN Sales.Customers PC
+	ON MC.BillToCustomerID = PC.CustomerID
+JOIN Sales.CustomerCategories CC
+	ON MC.CustomerCategoryID = CC.CustomerCategoryID
+
+
+/*####
+	Querry for extracting sale data -> factSale
+####*/
+SELECT 
+	I.InvoiceID,
+	I.CustomerID,
+	I.SalespersonPersonID,
+	C.DeliveryCityID 'LocationID',
+	I.InvoiceDate,
+	O.OrderDate,
+	O.ExpectedDeliveryDate,
+	I.ConfirmedDeliveryTime,
+	IL.StockItemID,
+	IL.Quantity,
+	IL.UnitPrice,
+	IL.TaxAmount,
+	IL.LineProfit
+FROM Sales.Invoices I
+JOIN Sales.InvoiceLines IL
+	ON I.InvoiceID = IL.InvoiceID
+JOIN Sales.Orders O
+	ON I.OrderID = O.OrderID
+JOIN Sales.Customers C
+	ON I.CustomerID = C.CustomerID
+
+/*####
+	Querry for extracting employee data -> dimEmployee
+####*/
+
+SELECT 
+	PersonID,
+	FullName,
+	IsSalesperson
+FROM Application.People
+WHERE IsEmployee = 1
+
+
+/*####
+	Querry for extracting location data -> dimLocation
+####*/
+SELECT 
+	C.CityID 'LocationID',
+	C.CityName,
+	SP.StateProvinceName,
+	SP.SalesTerritory,
+	CO.CountryName,
+	CO.Continent,
+	CO.Region,
+	CO.Subregion
+FROM Application.Cities C
+JOIN Application.StateProvinces SP
+	ON C.StateProvinceID = SP.StateProvinceID
+JOIN Application.Countries CO
+	ON SP.CountryID = CO.CountryID
+
+
+/*####
+	Querry for extracting stock items data -> dimStockItem
+####*/
+
+SELECT 
+	SI.StockItemID,
+	SI.StockItemName,
+	C.ColorName,
+	PT.PackageTypeName 'UnitPackage',
+	PTO.PackageTypeName 'OuterPackage',
+	SI.Size
+FROM Warehouse.StockItems SI
+LEFT JOIN Warehouse.Colors C
+	ON SI.ColorID = C.ColorID
+JOIN Warehouse.PackageTypes PT
+	ON SI.UnitPackageID = PT.PackageTypeID
+	JOIN Warehouse.PackageTypes PTO
+	ON SI.OuterPackageID = PTO.PackageTypeID
